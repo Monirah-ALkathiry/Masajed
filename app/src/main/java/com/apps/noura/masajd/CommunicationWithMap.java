1b52d0c46d388af7a42881a63a50acf2e037fca1
Monirah <iam.monerah@gmail.com>
2018-07-16 12:54:55 +0300

    google map : 1)- Map Idle. 2)- search . 3) close search.

---
+++ app/src/main/java/com/apps/noura/masajd/CommunicationWithMap.java
@@ -0,0 +1,10 @@
+package com.apps.noura.masajd;
+
+/**
+ * Created by Monirah on 7/15/2018.
+ */
+
+public interface CommunicationWithMap {
+    void receiveData(String m);
+
+}
---
+++ app/src/main/java/com/apps/noura/masajd/FirstFragmentListenerMAP.java
@@ -0,0 +1,11 @@
+package com.apps.noura.masajd;
+
+import java.util.List;
+
+/**
+ * Created by Monirah on 7/12/2018.
+ */
+
+public interface FirstFragmentListenerMAP {
+     void sendData(List<MosquesLatLng> m);
+}
