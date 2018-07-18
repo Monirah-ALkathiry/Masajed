cd8d1d545ed434bcd8355fb84050981eda12b9f2
Monirah <iam.monerah@gmail.com>
2018-07-18 15:48:05 +0300

    Dawa google map : 1)- Dawa Idle. 2)- search . 3) close search.

---
+++ app/src/main/java/com/apps/noura/masajd/FirstFragmentListenerDawaMAP.java
@@ -0,0 +1,12 @@
+package com.apps.noura.masajd;
+
+import java.util.List;
+
+/**
+ * Created by Monirah on 7/18/2018.
+ */
+
+public interface FirstFragmentListenerDawaMAP {
+    void sendData(List<DawaLatLng> m);
+
+}
